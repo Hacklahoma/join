b60644db3023fb27246cd0c87b6e754528c28eef
s-fletcher <sfletchertx@gmail.com>
2020-03-01 15:18:12 -0600

    Styling changes for mobile

--- src/components/Positions/index.js
+++ src/components/Positions/index.js
@@ -6,23 +6,29 @@ import positions from '../../config/positions.json'
 export class Positions extends Component {
 
     componentDidMount() {
+        this.updateHeight();
+        window.addEventListener("resize", this.updateHeight);
+    }
+
+    updateHeight() {
         // Setting grid height
-        var height = document.getElementsByClassName('grid')[0].offsetHeight;
-        var items = document.getElementsByClassName('Item');
+        document.getElementsByClassName("grid")[0].style.height = "auto";
+        var height = document.getElementsByClassName("grid")[0].offsetHeight;
+        var items = document.getElementsByClassName("Item");
         var current = 0;
-        for(var i = 0; i < items.length; i++) {
+        for (var i = 0; i < items.length; i++) {
             // Debug
-            // console.log(current + " " + height);
+            console.log(current + " " + height);
 
             // If the current height is more than half the container height, then break
-            if(current > (height/2)) {
+            if (current > height / 2) {
                 break;
             }
             // Add item height to current height
             current += items[i].offsetHeight;
         }
         // Set calculated height to production
-        document.getElementsByClassName('grid')[0].style.height = current + "px";
+        document.getElementsByClassName("grid")[0].style.height = current + "px";
     }
     
 
