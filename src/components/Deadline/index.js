cd51d5f3f4c3357aaddda4b0446eba7899e92a71
s-fletcher <sfletchertx@gmail.com>
2020-03-02 08:28:36 -0600

    Updated meta data

---
+++ src/components/Deadline/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+export class Deadline extends Component {
+    render() {
+        return (
+            <div className="Deadline">
+                Deadline
+            </div>
+        )
+    }
+}
+
+export default Deadline
