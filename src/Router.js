a8ffbd44e1a6120aaac83ac76c15fdeea325dab5
s-fletcher <sfletchertx@gmail.com>
2020-03-31 01:49:25 -0500

    Fixed oversight

--- src/Router.js
+++ src/Router.js
@@ -1,42 +1,46 @@
-import React, { Component } from "react";
-import Home from "./views/Home";
-import Page from "./views/Page";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import status from "./config/status.json";
+import React, { Component } from 'react'
+import Home from './views/Home'
+import Page from './views/Page'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import status from './config/status.json'
 
 export class Router extends Component {
-    constructor() {
-        super();
-        this.state = {
-            closed: false,
-        };
-    }
+  constructor() {
+      super();
+      this.state = {
+          closed: false,
+      };
+  }
 
-    componentDidMount() {
-        // Setting up deadline
-        var deadlineComponents = status["deadline"].split("/");
-        var month = deadlineComponents[0] - 1;
-        var day = deadlineComponents[1];
-        var year = deadlineComponents[2];
-        var date = new Date(year, month, day, 23, 59, 59);
-        var now = new Date(Date.now());
+  componentDidMount() {
+      // Setting up deadline
+      var deadlineComponents = status["deadline"].split("/");
+      var month = deadlineComponents[0] - 1;
+      var day = deadlineComponents[1];
+      var year = deadlineComponents[2];
+      var date = new Date(year, month, day, 23, 59, 59);
+      var now = new Date(Date.now());
 
-        // Setting date states
-        this.setState({
-            closed: date.getTime() < now.getTime(),
-        });
-    }
+      // Setting date states
+      this.setState({
+          closed: status["rolling"] ? false : date.getTime() < now.getTime(),
+      });
+  }
 
-    render() {
-        return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/:page" component={Page} />
-                </Switch>
-            </BrowserRouter>
-        );
-    }
+  render() {
+      return (
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/" component={Home} />
+                  {this.state.closed ? (
+                      <Route path="/:page" component={Home} />
+                  ) : (
+                      <Route path="/:page" component={Page} />
+                  )}
+              </Switch>
+          </BrowserRouter>
+      );
+  }
 }
 
-export default Router;
+export default Router
